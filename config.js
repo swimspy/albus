18158d7f5354240244327c1d5cc32235ebe76b70
avintas <avintas@gmail.com>
2016-07-26 13:38:04 -0700

    initial commit of full code

---
+++ config.js
@@ -0,0 +1,51 @@
+'use strict';
+
+exports.port = process.env.PORT || 3000;
+exports.mongodb = {
+  uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:27017/albus'
+};
+exports.companyName = 'swimspy.us';
+exports.projectName = 'sw-albus';
+exports.systemEmail = 'ai@swimspy.us';
+//exports.cryptoKey = 'k3yb0ardc4t';
+exports.cryptoKey = 'a3yb8ardi4t';
+exports.loginAttempts = {
+  forIp: 50,
+  forIpAndUser: 7,
+  logExpiration: '20m'
+};
+exports.requireAccountVerification = false;
+exports.smtp = {
+  from: {
+    name: process.env.SMTP_FROM_NAME || exports.projectName +' Website',
+    address: process.env.SMTP_FROM_ADDRESS || 'ai@swimpsy.us'
+  },
+  credentials: {
+    user: process.env.SMTP_USERNAME || 'ai@swimspy.us',
+    password: process.env.SMTP_PASSWORD || 'Planeta45',
+    host: process.env.SMTP_HOST || 'smtp.gmail.com',
+    ssl: true
+  }
+};
+exports.oauth = {
+  twitter: {
+    key: process.env.TWITTER_OAUTH_KEY || '',
+    secret: process.env.TWITTER_OAUTH_SECRET || ''
+  },
+  facebook: {
+    key: process.env.FACEBOOK_OAUTH_KEY || '',
+    secret: process.env.FACEBOOK_OAUTH_SECRET || ''
+  },
+  github: {
+    key: process.env.GITHUB_OAUTH_KEY || '',
+    secret: process.env.GITHUB_OAUTH_SECRET || ''
+  },
+  google: {
+    key: process.env.GOOGLE_OAUTH_KEY || '',
+    secret: process.env.GOOGLE_OAUTH_SECRET || ''
+  },
+  tumblr: {
+    key: process.env.TUMBLR_OAUTH_KEY || '',
+    secret: process.env.TUMBLR_OAUTH_SECRET || ''
+  }
+};
